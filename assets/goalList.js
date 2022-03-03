83e12bd01e89a6235aaeaf103f8b37b98b3088bb
Michael Souter <souterm1@gmail.com>
2022-03-02 21:43:49 -0500

    Goal selector and quote generator working, need to finalize styling until MVP Complete

--- assets/goalList.js
+++ assets/goalList.js
@@ -1,7 +1,25 @@
 const goalInput = document.getElementById("goal_input");
 const goalButton = document.getElementById("goal_button");
 const goalListParent = document.getElementById("goal_listParent");
+const fateButton = document.getElementById("fateButton");
+const goalText = document.getElementById("goalText");
+const motivateMe = document.getElementById("motivateMe");
+
 let goalList = [];
+
+function decideMyFate() {
+  let randomLength = goalList.length;
+  let random = Math.floor(Math.random() * randomLength);
+  let selectedGoal = goalList[random];
+  goalText.innerHTML = selectedGoal;
+}
+
+function generateQuote() {
+  let randomLength = inspiration.length;
+  let random = Math.floor(Math.random() * randomLength);
+  let selectedQuote = inspiration[random];
+  motivateMe.innerHTML = selectedQuote;
+}
 
 function updateLocalStorage(array) {
   localStorage.setItem("goals", JSON.stringify(array));
@@ -30,6 +48,13 @@ goalButton.addEventListener("click", function () {
   displayGoals(goalList);
   updateLocalStorage(goalList);
   goalInput.value = "";
+});
+
+fateButton.addEventListener("click", function () {
+  goalText.innerHTML = "";
+  motivateMe.innerHTML = "";
+  decideMyFate();
+  generateQuote();
 });
 
 init();
---
+++ assets/quote.js
@@ -0,0 +1,7 @@
+let inspiration = [
+  "Get after it!",
+  " Do or Do not. There is no try",
+  "Eat that elephant one bite at a time",
+  "Overwhelmed? Break your task into smaller sub tasks, defeat that bitch in detail",
+  "Discipline is the bridge between goals and accomplishments.",
+];
